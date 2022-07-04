6d3fc4ee2b930ebaba286a78937cdd1d939c2e6c
horanchikk <morozovvv2005@gmail.com>
2022-07-04 14:51:23 +0700

    added link to download, changed favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       dropShadow: {
-        '3xl': '0 35px 35px rgba(0, 0, 0, 0.8)'
-      }
+        "3xl": "0 35px 35px rgba(0, 0, 0, 0.8)",
+      },
     },
     screens: {
-      'tablet': '640px',
+      tablet: "640px",
       // => @media (min-width: 640px) { ... }
-      'laptop': '1024px',
+      laptop: "1024px",
       // => @media (min-width: 1024px) { ... }
-      'desktop': '1280px',
+      desktop: "1280px",
       // => @media (min-width: 1280px) { ... }
     },
   },
   plugins: [],
-}
+};
